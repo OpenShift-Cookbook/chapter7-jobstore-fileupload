e3d90fafe68b962130766a879d63739a8ca62f79
Shekhar Gulati <shekhargulati84@gmail.com>
2014-03-09 17:39:11 +0530

    basic view added for only read operations. working list all companies and all jobs for a company

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,76 @@
+(function($) {
+	var JobStore = {};
+	window.JobStore = JobStore;
+	
+	var template = function (name) {
+        return $('#' + name + '-template').html();
+    };
+
+	JobStore.IndexView = Backbone.View.extend({
+		render : function() {
+			$.ajax('api/v1/companies',{
+				method :'GET',
+				success : function(data){
+					if(null != data){
+						_.each(data,function(json){
+							var companyHTML = Mustache.to_html(template("company"), json);
+                            $("#companies").append(companyHTML);
+						});
+					}
+				}
+			});
+		}
+	});
+	
+	JobStore.JobView = Backbone.View.extend({
+		initialize : function(options){
+			this.companyId = options.companyId;
+		},
+		
+		render : function(){
+			$.ajax('api/v1/companies/'+this.companyId+'/jobs',{
+				method : 'GET',
+				success : function(data){
+					if(null != data){
+						_.each(data, function(json){
+							var jobHTML = Mustache.to_html(template("job"), json);
+                            $("#jobs").append(jobHTML);
+						});
+					}
+				}
+			});
+		}
+	});
+
+	JobStore.Router = Backbone.Router.extend({
+		currentView : null,
+
+		routes : {
+			"" : "showAllCompanies",
+			":companyId/jobs" : "jobsForACompany"
+		},
+
+		changeView : function(view) {
+			if (null != this.currentView) {
+				this.currentView.undelegateEvents();
+			}
+			this.currentView = view;
+			this.currentView.render();
+		},
+
+		showAllCompanies : function() {
+			console.log("in showAllCompanies()...");
+			this.changeView(new JobStore.IndexView());
+		},
+
+		jobsForACompany : function(companyId) {
+			console.log("in jobsForACompany()...");
+			this.changeView(new JobStore.JobView({companyId: companyId}));
+		}
+
+	});
+
+	var router = new JobStore.Router();
+	Backbone.history.start();
+
+})(jQuery);
